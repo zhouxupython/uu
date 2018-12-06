54349f92a8cde56156ca42023bc7bb25606820d5
zhouxupython <james19840406@163.com>
2018-12-07 00:42:49 +0800

    1

---
+++ upload3.sh
@@ -0,0 +1,300 @@
+#!/bin/bash
+
+echo "begin"
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.001 ./
+git add .
+git commit -m "1"
+git push
+
+echo "--------1 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.002 ./
+git add .
+git commit -m "2"
+git push
+
+echo "--------2 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.003 ./
+git add .
+git commit -m "3"
+git push
+
+echo "--------3 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.004 ./
+git add .
+git commit -m "4"
+git push
+
+echo "--------4 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.005 ./
+git add .
+git commit -m "5"
+git push
+
+echo "--------5 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.006 ./
+git add .
+git commit -m "6"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.007 ./
+git add .
+git commit -m "7"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.008 ./
+git add .
+git commit -m "8"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.009 ./
+git add .
+git commit -m "9"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.010 ./
+git add .
+git commit -m "10"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.011 ./
+git add .
+git commit -m "11"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.012 ./
+git add .
+git commit -m "12"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.013 ./
+git add .
+git commit -m "13"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.014 ./
+git add .
+git commit -m "14"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.015 ./
+git add .
+git commit -m "15"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.016 ./
+git add .
+git commit -m "16"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.017 ./
+git add .
+git commit -m "17"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.018 ./
+git add .
+git commit -m "18"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.019 ./
+git add .
+git commit -m "19"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.020 ./
+git add .
+git commit -m "20"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.021 ./
+git add .
+git commit -m "21"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.022 ./
+git add .
+git commit -m "22"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.023 ./
+git add .
+git commit -m "23"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.024 ./
+git add .
+git commit -m "24"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.025 ./
+git add .
+git commit -m "25"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.026 ./
+git add .
+git commit -m "26"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.027 ./
+git add .
+git commit -m "27"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.028 ./
+git add .
+git commit -m "28"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.029 ./
+git add .
+git commit -m "29"
+git push
+
+echo "--------009 over-------"
+
+sleep 5
+
+
+mv /home/zx/resource/ref-resources/spilt/ref-resources.zip.030 ./
+git add .
+git commit -m "30"
+git push
+
+echo "--------all over-------"
+
+
